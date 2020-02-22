82df2acd295b55ab30a92a63dbaf39d55464bcb6
Meesa Shivaram Prasad <55886366+codemaster-22@users.noreply.github.com>
2020-02-22 22:15:32 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+int main() {
+    int n;
+    cin>>n;
+    int p;
+    cin>>p;
+    hotel H(p);
+    while(p--)
+    {
+     int i;
+     cin>>i;
+     H.setlimit(i);
+    }
+    while(n--)
+    {
+     int a,b;
+     cin>>a>>b;
+     switch(a)
+     {
+         case 1:H.insert(b);
+                break;
+         case 2:H.search(b);
+                break;
+     }
+    }
+    return 0;
+}

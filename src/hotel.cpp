acdfc33d51e0e487143292f44378766f3a89b9d8
Meesa Shivaram Prasad <55886366+codemaster-22@users.noreply.github.com>
2020-02-22 22:17:08 +0530

    Create hotel.cpp

---
+++ src/hotel.cpp
@@ -0,0 +1,84 @@
+#include "../include/hotel.h"
+void room::print()
+{
+    for(auto it:V)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
+class hotel{
+    public:
+    vector<room*> P;
+    int room_count;
+    void setlimit(int n);
+    void insert(int patient);
+    bool search(int patient,int k);
+    hotel(int l):room_count(l){}
+};
+void hotel::setlimit(int n)
+{
+    room* ptr=new room(n);
+    P.push_back(ptr);
+}
+void hotel::insert(int patient)
+{
+    if(search(patient,0)){
+        cout<<-1<<endl;
+        return;
+    }
+    int j=(patient%room_count);
+    room* r=P[j];
+    if(r->V.size()<r->limit){
+        r->V.push_back(patient);
+        return;
+    }
+    int i=(j+1)%room_count;
+    while(i!=j)
+    {    
+        r=P[i];
+        if(r->V.size()<r->limit)
+        {
+            r->V.push_back(patient);
+            return;
+        }
+        i++;
+        i=i%(room_count);
+    }
+    cout<<-1<<endl;
+}
+bool find(room* ptr,int patient)
+{
+    std::vector<int>::iterator it;
+    it=std::find(ptr->V.begin(),ptr->V.end(),patient);
+    if(it==ptr->V.end())
+        return false;
+    return true;
+}
+bool hotel::search(int patient,int k=1)
+{
+    int j=(patient%room_count);
+    room* r=P[j];
+    if(find(r,patient))
+    {
+        if(k==1)
+            r->print();
+        return true;
+    }
+    int i=(j+1)%room_count;
+    while(i!=j)
+    {
+        r=P[i];
+        if(find(r,patient))
+        {
+            if(k==1)
+                r->print();
+            return true;
+        }
+        i++;
+        i=i%(room_count);
+    }
+    if(k==1)
+        cout<<-1<<endl;
+    return false;
+}
